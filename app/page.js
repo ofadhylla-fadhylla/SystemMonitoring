bbd42939694cd275db24b3df319361353746a4ed
ofadhylla-fadhylla <ofadhylla@gmail.com>
2026-09-13 23:46:25 +0700

    Add data quality center to dashboard

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
 import DashboardV2 from '../components/DashboardV2';
 import ExecutiveInsightPanel from '../components/ExecutiveInsightPanel';
 import PortfolioAnalyticsPanel from '../components/PortfolioAnalyticsPanel';
+import DataQualityPanel from '../components/DataQualityPanel';
 
 export default function HomePage(){
-  return <><DashboardV2/><ExecutiveInsightPanel/><PortfolioAnalyticsPanel/></>;
+  return <><DashboardV2/><ExecutiveInsightPanel/><PortfolioAnalyticsPanel/><DataQualityPanel/></>;
 }
