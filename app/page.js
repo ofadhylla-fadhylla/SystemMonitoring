83bf16fc9a3077e3088af00d7fd1c60f59e27bec
ofadhylla-fadhylla <ofadhylla@gmail.com>
2026-09-13 23:07:16 +0700

    Add portfolio heatmap and risk trend to dashboard

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import DashboardV2 from '../components/DashboardV2';
 import ExecutiveInsightPanel from '../components/ExecutiveInsightPanel';
+import PortfolioAnalyticsPanel from '../components/PortfolioAnalyticsPanel';
 
 export default function HomePage(){
-  return <><DashboardV2/><ExecutiveInsightPanel/></>;
+  return <><DashboardV2/><ExecutiveInsightPanel/><PortfolioAnalyticsPanel/></>;
 }
