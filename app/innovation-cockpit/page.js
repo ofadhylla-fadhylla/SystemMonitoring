5a381d7c53ea9a1a792454732c7d1a38fda5205a
ofadhylla-fadhylla <ofadhylla@gmail.com>
2026-09-16 14:41:22 +0700

    Add standalone Innovation Cockpit route

---
+++ app/innovation-cockpit/page.js
@@ -0,0 +1,10 @@
+import InnovationCockpit from '../../components/InnovationCockpit';
+
+export const metadata = {
+  title: 'Innovation Cockpit | SMD',
+  description: 'Executive innovation cockpit for SMD sustainability monitoring and competition demo.',
+};
+
+export default function InnovationCockpitPage(){
+  return <InnovationCockpit/>;
+}
