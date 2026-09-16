a7a63ae0bed8319e4a39a7cb11f0d59652ae90e0
ofadhylla-fadhylla <ofadhylla@gmail.com>
2026-09-16 14:41:38 +0700

    Separate Innovation Cockpit from main dashboard

--- app/page.js
+++ app/page.js
@@ -1,9 +1,8 @@
-import InnovationCockpit from '../components/InnovationCockpit';
 import DashboardV2 from '../components/DashboardV2';
 import ExecutiveInsightPanel from '../components/ExecutiveInsightPanel';
 import PortfolioAnalyticsPanel from '../components/PortfolioAnalyticsPanel';
 import DataQualityPanel from '../components/DataQualityPanel';
 
 export default function HomePage(){
-  return <><InnovationCockpit/><DashboardV2/><ExecutiveInsightPanel/><PortfolioAnalyticsPanel/><DataQualityPanel/></>;
+  return <><DashboardV2/><ExecutiveInsightPanel/><PortfolioAnalyticsPanel/><DataQualityPanel/></>;
 }
