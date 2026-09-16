61d22fe9bf0d2e7167b0177b5ba00c21e28170b4
ofadhylla-fadhylla <ofadhylla@gmail.com>
2026-09-16 14:30:33 +0700

    Show innovation cockpit above executive dashboard

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
+import InnovationCockpit from '../components/InnovationCockpit';
 import DashboardV2 from '../components/DashboardV2';
 import ExecutiveInsightPanel from '../components/ExecutiveInsightPanel';
 import PortfolioAnalyticsPanel from '../components/PortfolioAnalyticsPanel';
 import DataQualityPanel from '../components/DataQualityPanel';
 
 export default function HomePage(){
-  return <><DashboardV2/><ExecutiveInsightPanel/><PortfolioAnalyticsPanel/><DataQualityPanel/></>;
+  return <><InnovationCockpit/><DashboardV2/><ExecutiveInsightPanel/><PortfolioAnalyticsPanel/><DataQualityPanel/></>;
 }
